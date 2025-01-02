84d5e7eebad344ad1732256fa6b0a2b0ea6e0da5
Andrei Kretov <akretov@c1r5s7.42prague.com>
2025-01-02 17:09:22 +0100

    Starting with C++

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akretov <akretov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 16:19:50 by akretov           #+#    #+#             */
+/*   Updated: 2025/01/02 17:06:49 by akretov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2) {
+        for (int i = 0; argv[1][i]; ++i)
+            std::cout << (char)std::toupper(argv[1][i]);
+        std::cout << std::endl;  
+    } else {
+        std::cout << "There should be only 1 argument" << std::endl;
+    }
+    return 0;
+}
