20b3c865862d87a2c1664982ce759af5bd7a9421
Andrei Kretov <akretov@c2r2s5.42prague.com>
2025-01-04 16:03:14 +0100

    test

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,18 +6,21 @@
 /*   By: akretov <akretov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:50:54 by akretov           #+#    #+#             */
-/*   Updated: 2025/01/03 18:02:56 by akretov          ###   ########.fr       */
+/*   Updated: 2025/01/04 16:02:42 by akretov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-class   Contact {
+class Contact {
     private:
         std::string name;
         std::string phoneNumber;
+
     public:
-        Contact(std::string name = "", std::string phoneNumber = ""){
-            
-        }
-}
+        // Constructor with different parameter names
+        Contact(std::string contactName = "", std::string contactPhoneNumber = "") {
+            name = contactName;           // No ambiguity, so no need for 'this->'
+            phoneNumber = contactPhoneNumber; // No ambiguity
+    }
+};
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: akretov <akretov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 16:22:04 by akretov           #+#    #+#             */
-/*   Updated: 2025/01/03 17:48:29 by akretov          ###   ########.fr       */
+/*   Updated: 2025/01/04 16:02:57 by akretov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,17 @@ std::map<std::string, int>  create_command_map();
 //process_input
 void                        display_prompt();
 std::string                 get_user_input();
+
+//class
+class Contact {
+    private:
+        std::string name;
+        std::string phoneNumber;
+
+    public:
+        // Constructor with different parameter names
+        Contact(std::string contactName = "", std::string contactPhoneNumber = "") {
+            name = contactName;           // No ambiguity, so no need for 'this->'
+            phoneNumber = contactPhoneNumber; // No ambiguity
+    }
+};
