8c0dad173a54a0a0a54578f5b8ec285264efed9f
Andrei Kretov <akretov@student.42prague.com>
2025-01-12 19:12:49 +0100

    adding more parameters to megaphone in ex00. Argc could be more than 2

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: akretov <akretov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:19:50 by akretov           #+#    #+#             */
-/*   Updated: 2025/01/04 19:34:36 by akretov          ###   ########.fr       */
+/*   Updated: 2025/01/12 19:11:55 by akretov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 #include <cctype>
 
 int main(int argc, char *argv[]) {
-    if (argc == 2) {
-        for (int i = 0; argv[1][i]; ++i)
-            std::cout << (char)std::toupper(argv[1][i]);
+    if (argc > 2) {
+        for (int j = 1; j < argc; j++) {
+            for (int i = 0; argv[j][i]; ++i) {
+                std::cout << (char)std::toupper(argv[j][i]);
+            }
+            std::cout << " ";
+        }
         std::cout << std::endl;  
     } else {
-       std:: cout << "There should be only 1 argument" << std::endl;
+       std:: cout << "There should be at least 1 argument" << std::endl;
     }
     return 0;
 }
